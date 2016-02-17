d9b9a574c52652a0ec19e5e11944b7ab6482bb9a
Cody Condon <codycondon50@gmail.com>
2016-02-16 19:04:00 -0500

    initial commit

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,26 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+// $(document).on('page:load ready', function() {
+//   function display_search_results() {
+//     // readyState === 4 means that the asynchronous request completed successfully
+//     if (this.readyState === 4) {
+//       console.log(this);
+//       document.getElementById('products').innerHTML = this.responseText;
+//     }
+//   }
+//
+//   var form = document.getElementById('search-form');
+//
+//   form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//
+//     var searchValue = document.getElementById('search').value;
+//
+//     var xhr = new XMLHttpRequest();  // XHR
+//     xhr.onload = display_search_results;
+//     xhr.open('GET', '/products?search=' + searchValue, true);
+//     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+//     xhr.send();
+//   });
+});
